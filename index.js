6438b9d8b067e0cf2c9a21c78d039f3c30764f09
Adesh Katiya <adeshkatiya27@gmail.com>
2024-12-24 19:05:30 +0530

    -> removed default controls -> added initial and final song times -> css improvements

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const song = document.getElementById('song');
 const range = document.getElementById('range');
 const ctrlIcon = document.getElementById('ctrlIcon');
+const songInitialTime = document.getElementById('songInitialTime');
+const songFinalTime = document.getElementById('songFinalTime');
 
 song.onloadedmetadata = function () {
     range.max = song.duration;
     range.value = song.currentTime;
-    updateRangeBackground();
 };
 
 console.log(song.duration);
@@ -23,22 +24,39 @@ function pauseSong() {
     }
 }
 
-function updateRangeBackground() {
-    const playedPercent = (range.value / range.max) * 100;
-    range.style.backgroundColor = `linear-gradient(to right,#003049 ${playedPercent}, #4caf50 ${playedPercent})`;
-}
-
 if (song.play()) {
     setInterval(() => {
         range.value = song.currentTime;
-    }, 5000);
-    console.log(range.max);
+        seekUpdate();
+    }, 500);
+    console.log(song);
+}
+
+function seekUpdate() {
+    let seekPosition = 0;
+
+    if (!isNaN(song.duration)) {
+        seekPosition = song.currentTime * (100 / song.duration);
+        range.value = seekPosition;
+
+        let currentMinutes = Math.floor(song.currentTime / 60);
+        let currentSeconds = Math.floor(song.currentTime - currentMinutes * 60);
+        let durationMinutes = Math.floor(song.duration / 60);
+        let durationSeconds = Math.floor(song.duration - durationMinutes * 60);
+
+        if (currentSeconds < 10) { currentSeconds = "0" + currentSeconds; }
+        if (durationSeconds < 10) { durationSeconds = "0" + durationSeconds; }
+        if (currentMinutes < 10) { currentMinutes = "0" + currentMinutes; }
+        if (durationMinutes < 10) { durationMinutes = "0" + durationMinutes; }
+
+        songInitialTime.textContent = currentMinutes + ":" + currentSeconds;
+        songFinalTime.textContent = durationMinutes + ":" + durationSeconds;
+    }
 }
 
 range.onchange = function () {
     song.play();
     song.currentTime = range.value;
-    updateRangeBackground();
 };
 
 song.pause();
